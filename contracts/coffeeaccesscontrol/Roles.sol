c00e788d2ac01462010a2f7c938e7413f689a184
Murilo Ito <murilo.ito@mercadobitcoin.com.br>
2019-10-13 17:46:59 -0300

    Initial commit

---
+++ contracts/coffeeaccesscontrol/Roles.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.5.8;
+
+/**
+ * @title Roles
+ * @dev Library for managing addresses assigned to a Role.
+ */
+library Roles {
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  /**
+   * @dev give an account access to this role
+   */
+  function add(Role storage role, address account) internal {
+    require(account != address(0), "You are not the owner");
+    require(!has(role, account), "You are not allowed");
+
+    role.bearer[account] = true;
+  }
+
+  /**
+   * @dev remove an account's access to this role
+   */
+  function remove(Role storage role, address account) internal {
+    require(account != address(0), "You are not the owner");
+    require(has(role, account), "You are not allowed");
+
+    role.bearer[account] = false;
+  }
+
+  /**
+   * @dev check if an account has this role
+   * @return bool
+   */
+  function has(Role storage role, address account)
+    internal
+    view
+    returns (bool)
+  {
+    require(account != address(0), "You are not the owner");
+    return role.bearer[account];
+  }
+}
